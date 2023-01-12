561f31450e6bec9ba5a14ba3c36f464aa760eab6
MogioR <egormog00@bk.ru>
2023-01-12 17:10:16 +0300

    fixes

--- src/gate_way_service/blueprints/delete_rental_blueprint.py
+++ src/gate_way_service/blueprints/delete_rental_blueprint.py
@@ -21,7 +21,7 @@ async def delete_rental(rentalUid: str) -> Response:
                 'errors': ['Rental service is unavailable.']
             })
         )
-    elif response.status_code % 100 != 2:
+    elif int(response.status_code / 100) != 2:
         return Response(
             status=response.status_code,
             content_type='application/json',
